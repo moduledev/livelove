37a060ef98c471e177ab09af543782062ef92b24
vagrant <vagrant@homestead>
2019-04-23 18:02:41 +0000

    install laravel pasport, created forceJsonResponse

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|min:9',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $user = User::create($request->toArray());
+
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+
+        return response($response, 200);
+    }
+
+    public function login (Request $request) {
+
+        $user = User::where('phone', $request->phone)->first();
+
+        if ($user) {
+
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = "Password missmatch";
+                return response($response, 422);
+            }
+
+        } else {
+            $response = 'User does not exist';
+            return response($response, 422);
+        }
+
+    }
+}
