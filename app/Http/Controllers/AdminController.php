8bf0f799e4be9a268a14b60ea0517b3c15c44c2c
lion1121 <vagrant@homestead>
2019-05-02 07:44:50 +0000

    fix admin register

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,7 +11,7 @@ class AdminController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:admin')->except('create');
+        $this->middleware('auth:admin')->except('create','store');
     }
 
     /**
